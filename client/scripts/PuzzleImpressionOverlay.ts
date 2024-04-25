ce58942904ca7c1985f1fb5b09df2aa4eae3e7a8
Dan Hems <danhems87@gmail.com>
2024-04-25 18:46:54 +0100

    Image crop overlay drag logic for Puzzly Creator abstracted to RestrictedDraggable class.

--- client/scripts/PuzzleImpressionOverlay.ts
+++ client/scripts/PuzzleImpressionOverlay.ts
@@ -1,133 +1,56 @@
+import RestrictedDraggable from "./RestrictedDraggable";
 import { MovementAxis, PuzzleShapes } from "./types";
 
 export default class PuzzleImpressionOverlay { 
-    element: HTMLDivElement;
     targetElement: HTMLImageElement | HTMLDivElement;
     container: HTMLElement;
-    width: number;
-    height: number;
-    diffX: number;
-    diffY: number;
-    leftBoundary: number;
-    rightBoundary: number;
-    topBoundary: number;
-    bottomBoundary: number;
-    allowedMovementAxis: MovementAxis | undefined;
     isSquareOptionSelected: boolean = true;
-    isMoving: boolean = false;
 
     constructor({ targetElement, isSquareOptionSelected }: { targetElement: HTMLImageElement | HTMLDivElement; isSquareOptionSelected: true }) {
         this.targetElement = targetElement;
         this.isSquareOptionSelected = isSquareOptionSelected;
         this.container = this.targetElement.parentElement as HTMLElement;
 
-        this.setInitialValues({ isSquareOptionSelected: this.isSquareOptionSelected });
-        this.element = this.createElement();
-        this.container.append(this.element);
+        const layout = this.getInitialValues({ isSquareOptionSelected: this.isSquareOptionSelected });
 
-        this.attachListeners();
+        new RestrictedDraggable({ containerElement: this.container, layout, id: "puzzle-impression-overlay", restrictionBoundingBox: layout });
     }
 
-    setInitialValues({ isSquareOptionSelected }: { isSquareOptionSelected: boolean }) {
-        let width, height;
+    getInitialValues({ isSquareOptionSelected }: { isSquareOptionSelected: boolean }) {
+        let width, height, allowedMovementAxis;
+
+        // Calculate top and left position of target element, assuming it is centered
+        const topBoundary = (this.container.offsetHeight - this.targetElement.offsetHeight) / 2;
+        const leftBoundary = (this.container.offsetWidth - this.targetElement.offsetWidth) / 2;
+        const rightBoundary = this.container.offsetWidth - leftBoundary;
+        const bottomBoundary = this.container.offsetHeight - topBoundary;
 
         if(isSquareOptionSelected){
             const size = Math.min(this.targetElement.offsetWidth, this.targetElement.offsetHeight);
             width = height = size;
 
             if(this.targetElement.offsetWidth < this.targetElement.offsetHeight){
-                this.allowedMovementAxis = MovementAxis.Y;
+                allowedMovementAxis = MovementAxis.Y;
             } else {
-                this.allowedMovementAxis = MovementAxis.X;
+                allowedMovementAxis = MovementAxis.X;
             }
         } else {
             width = this.targetElement.offsetWidth;
             height = this.targetElement.offsetHeight;
         }
 
-        this.width = width;
-        this.height = height;
-    }
-
-    createElement() {
-        const element = document.createElement("div");
-        element.id = "puzzle-impression-overlay";
-        element.style.position = "absolute";
-
-        // Calculate top and left position of target element, assuming it is centered
-        this.topBoundary = (this.container.offsetHeight - this.targetElement.offsetHeight) / 2;
-        this.leftBoundary = (this.container.offsetWidth - this.targetElement.offsetWidth) / 2;
-        this.rightBoundary = this.container.offsetWidth - this.leftBoundary;
-        this.bottomBoundary = this.container.offsetHeight - this.topBoundary;
-
-        element.style.top = this.topBoundary + "px";
-        element.style.left = this.leftBoundary + "px";
-        element.style.width = this.width + "px";
-        element.style.height = this.height + "px";
-        return element;
-    }
-
-    attachListeners() {
-        this.element.addEventListener("mousedown", this.onMouseDown.bind(this))
-    }
-
-    onMouseDown(e: MouseEvent) {
-        this.diffX = e.clientX - parseInt(this.element.style.left);
-        this.diffY = e.clientY - parseInt(this.element.style.top);
-        this.element.addEventListener("mousemove", this.onMouseMove.bind(this));
-        this.element.addEventListener("mouseup", this.onMouseUp.bind(this));
-
-        this.isMoving = true;
-    }
-
-    onMouseMove(e: MouseEvent) {
-        if(this.isMoving){
-            console.log("mouse is moving")
-            
-            const 
-
-            if(this.allowedMovementAxis === MovementAxis.X) {
-                const newValue = e.clientX - this.diffX;
-                if(newValue >= this.leftBoundary || newValue <= this.rightBoundary) {
-                    this.element.style.left = e.clientX - this.diffX + "px";
-                }
-            } else if(this.allowedMovementAxis === MovementAxis.Y) {
-                const newValue = e.clientY - this.diffY;
-                if(newValue >= this.topBoundary || newValue <= this.bottomBoundary) {
-                    this.element.style.top = e.clientY - this.diffY + "px";
-                }
-            } else {
-                this.element.style.left = e.clientX - this.diffX + "px";
-                this.element.style.top = e.clientY - this.diffY + "px";
-            }
-        }
-    }
-
-    onMouseUp(e: MouseEvent) {
-        if(this.isMoving){
-            this.isMoving = false;
-            this.element.removeEventListener("mousemove", this.onMouseMove);
+        return {
+            left: leftBoundary,
+            top: topBoundary,
+            right: rightBoundary,
+            bottom: bottomBoundary,
+            width,
+            height,
+            allowedMovementAxis,
         }
     }
 
     onShapeChange(shape: PuzzleShapes) {
 
-    }
-
-    isWithinBounds() {
-        // const elBoundingBox = {
-        //     top: newY,
-        //     right: newX + this.targetElement.offsetWidth,
-        //     bottom: newY + this.targetElement.offsetHeight,
-        //     left: newX,
-        // };
-        // const containerBoundingBox = this.targetElement.getBoundingClientRect();
-        
-        // return (
-        //     elBoundingBox.left >= Math.ceil(containerBoundingBox.left) &&
-        //     elBoundingBox.right <= Math.ceil(containerBoundingBox.right) &&
-        //     elBoundingBox.top >= Math.ceil(containerBoundingBox.top) &&
-        //     elBoundingBox.bottom <= Math.ceil(containerBoundingBox.bottom)
-        // );
     }
 }
--- client/scripts/RestrictedDraggable.ts
+++ client/scripts/RestrictedDraggable.ts
@@ -1,16 +1,120 @@
+import { MovementAxis } from "./types";
+
+export interface RestrictedDraggableArguments {
+    containerElement: HTMLElement;
+    layout: {
+        top: number;
+        left: number;
+        width: number;
+        height: number;
+        allowedMovementAxis: MovementAxis | undefined
+    };
+    id: string;
+    restrictionBoundingBox: {
+        top: number;
+        right: number;
+        bottom: number;
+        left: number;
+    } | undefined
+};
+
 export default class RestrictedDraggable {
     element: HTMLElement;
+    containerElement: HTMLElement;
     topBoundary: number;
     rightBoundary: number;
     bottomBoundary: number;
     leftBoundary: number;
-    restrictToParent: boolean;
-    
+    isMoving: boolean = false;
+    diffX: number;
+    diffY: number;
+    allowedMovementAxis: MovementAxis | undefined;
 
-    constructor({ element, restrictToParent}: {element: HTMLElement; restrictToParent: boolean; }) {
-        this.element = element;
-        this.restrictToParent = restrictToParent;
+    constructor({
+        containerElement,
+        layout,
+        id,
+        restrictionBoundingBox 
+    }: RestrictedDraggableArguments) {
+        this.containerElement = containerElement;
+        this.element = this.createElement(id, layout);
+        this.containerElement.append(this.element);
+        this.allowedMovementAxis = layout.allowedMovementAxis;
+        this.setDragBounds(restrictionBoundingBox);
+        this.attachListeners();
+    }
 
+    createElement(id: string, layout: { top: number; left: number; width: number; height: number; }) {
+        const element = document.createElement("div");
+        element.id = id;
+        element.classList.add("restricted-draggable");
+        element.style.position = "absolute";
+        element.style.top = layout.top + "px";
+        element.style.left = layout.left + "px";
+        element.style.width = layout.width + "px";
+        element.style.height = layout.height + "px";
+        return element;
+    }
 
+    setDragBounds(restrictionBoundingBox: { top: number; right: number; bottom: number; left: number; } | undefined) {
+        if(restrictionBoundingBox) {
+            this.topBoundary = restrictionBoundingBox.top;
+            this.rightBoundary = restrictionBoundingBox.right;
+            this.bottomBoundary = restrictionBoundingBox.bottom;
+            this.leftBoundary = restrictionBoundingBox.left;
+        } else {
+            this.topBoundary = 0;
+            this.rightBoundary = this.containerElement.offsetWidth;
+            this.bottomBoundary = this.containerElement.offsetHeight;
+            this.leftBoundary = 0;
+        }
+    }
+
+    attachListeners() {
+        this.element.addEventListener("mousedown", this.onMouseDown.bind(this))
+    }
+
+    onMouseDown(e: MouseEvent) {
+        this.diffX = e.clientX - parseInt(this.element.style.left);
+        this.diffY = e.clientY - parseInt(this.element.style.top);
+        this.element.addEventListener("mousemove", this.onMouseMove.bind(this));
+        this.element.addEventListener("mouseup", this.onMouseUp.bind(this));
+
+        this.isMoving = true;
+    }
+
+    onMouseMove(e: MouseEvent) {
+        if(this.isMoving){
+            console.log("mouse is moving")
+
+            const newXValue = e.clientX - this.diffX;
+            const newYValue = e.clientY - this.diffY;
+
+            const isWithinHorizontalBounds = newXValue >= this.leftBoundary && newXValue + this.element.offsetWidth <= this.rightBoundary;
+
+            const isWithinVerticalBounds = newYValue >= this.topBoundary || newYValue + this.element.offsetHeight <= this.bottomBoundary;
+
+            if(this.allowedMovementAxis === MovementAxis.X) {
+                if(isWithinHorizontalBounds) {
+                    this.element.style.left = newXValue + "px";
+                }
+            } else if(this.allowedMovementAxis === MovementAxis.Y) {
+                if(isWithinVerticalBounds) {
+                    this.element.style.top = e.clientY - this.diffY + "px";
+                }
+            } else {
+                if(isWithinHorizontalBounds && isWithinVerticalBounds){
+                    this.element.style.left = e.clientX - this.diffX + "px";
+                    this.element.style.top = e.clientY - this.diffY + "px";
+                }
+            }
+        }
+    }
+
+    onMouseUp(e: MouseEvent) {
+        if(this.isMoving){
+            this.isMoving = false;
+            this.element.removeEventListener("mousemove", this.onMouseMove);
+        }
     }
 }
